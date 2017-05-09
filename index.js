3d3e2989574a278dd25f55e44eca6e90338b133b
SpencerSwell <sswell@live.com>
2017-05-09 16:58:52 -0400

    Minor adjustments Jquery started

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+var stateObject = [
+{	name:LegalEase,
+ 	appshowcase:'./pictures/legalease.png',
+ 
+ 	appdesc:`Legal Ease is a Web App
+  		that gives people the ability to
+   		translate basic english words to
+    	their legal definitions, allowing
+     	a regular citizen to make a basic
+      	legal statement if needed.`,
+   
+   lessons:`Don’t write code in a rushed manner,
+	    slow down and write things out beforehand
+	     and the project will be finished quicker
+	      and the code quality will be higher.
+	       The time to implement new features is
+	        often longer than you would expect and
+	         you should plan for some unexpected
+	          difficulty to appear.`,
+	
+	next:`Build out the functinallity
+	 to convert an entire phrase instead
+	  of just a word and also add many more
+	   words to the database, probably building
+	    a companion app to do so.`,
+
+
+  },
+{}
+{}]
+//event handlers
+
+$('.next').on('click', () => {
+
+})
