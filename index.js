ea1122a29e8dddd5c648b5df80f4f3b0460f9eec
SpencerSwell <sswell@live.com>
2017-05-10 03:10:53 -0400

    added jquery conditional rednering, some responsiveness

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
+$( document ).ready(function() {
+$('.legal-container').hide("slow")
+$('.chuck-container').hide("slow")
+$('.quiz-container').hide("slow")
+$('.diamonds-container').show("slow")
+$('.me').fadeIn(40000)
+});
 
 
-var stateObject = [
-{	name:LegalEase,
- 	appshowcase:'./pictures/legalease.png',
- 
- 	appdesc:`Legal Ease is a Web App
-  		that gives people the ability to
-   		translate basic english words to
-    	their legal definitions, allowing
-     	a regular citizen to make a basic
-      	legal statement if needed.`,
-   
-   lessons:`Don’t write code in a rushed manner,
-	    slow down and write things out beforehand
-	     and the project will be finished quicker
-	      and the code quality will be higher.
-	       The time to implement new features is
-	        often longer than you would expect and
-	         you should plan for some unexpected
-	          difficulty to appear.`,
-	
-	next:`Build out the functinallity
-	 to convert an entire phrase instead
-	  of just a word and also add many more
-	   words to the database, probably building
-	    a companion app to do so.`,
+//When you click on 21 Diamonds it will hide the other projects and show 21 Diamonds
+$('.tdd').on('click', () => {
+$('.legal-container').hide("slow")
+$('.chuck-container').hide("slow")
+$('.quiz-container').hide("slow")
+$('.diamonds-container').show("slow")
+})
 
+//When you click on Chuck norris jokes hide other projects and show that one
 
-  },
-{}
-{}]
-//event handlers
+$('.facts').on('click', () => {
+$('.legal-container').hide("slow")
+$('.quiz-container').hide("slow")
+$('.diamonds-container').hide("slow")
+$('.chuck-container').show("slow")
 
-$('.next').on('click', () => {
+})
+
+$('.dom').on('click', () => {
+$('.legal-container').hide("slow")
+$('.diamonds-container').hide("slow")
+$('.chuck-container').hide("slow")
+$('.quiz-container').show("slow")
+
+})
+
+$('.legal').on('click', () => {
+$('.diamonds-container').hide("slow")
+$('.chuck-container').hide("slow")
+$('.quiz-container').hide("slow")
+$('.legal-container').show("slow")
+
 
 })
